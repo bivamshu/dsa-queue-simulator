e3c575af90c8321938496c01e86dd0f95e3d86b4
bivamshu <bivamshu.khadka86@gmail.com>
2025-02-28 17:58:45 +0545

    structures

---
+++ src/traffic_simulation.c
@@ -0,0 +1,44 @@
+#ifndef TRAFFIC_SIMULATION_H
+#define TRAFFIC_SIMULATION_H
+
+#include <SDL.h>
+#include <stdbool.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define LANE_WIDTH 80
+#define MAX_VEHICLES 10
+#define INTERSECTION_X (WINDOW_WIDTH / 2)
+#define INTERSECTION_Y (WINDOW_HEIGHT / 2)
+
+typedef enum {
+    DIRECTION_NORTH,
+    DIRECTION_SOUTH,
+    DIRECTION_EAST,
+    DIRECTION_WEST
+} Direction;
+
+typedef enum {
+    TURN_NONE,
+    TURN_LEFT,
+    TURN_RIGHT
+} TurnDirection;
+
+typedef enum {
+    STATE_MOVING,
+    STATE_STOPPING,
+    STATE_STOPPED,
+    STATE_TURNING
+} VehicleState;
+
+typedef enum {
+    REGULAR_CAR,
+    AMBULANCE,
+    POLICE_CAR,
+    FIRE_TRUCK
+} VehicleType;
+
+typedef enum {
+    RED,
+    GREEN
+} TrafficLightState;
